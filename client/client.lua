370612d9729316bbddb3f0326452ce89998ea52f
GeekGarage <info@geekgarage.dk>
2022-12-27 03:51:42 +0100

    addded speed and stamina fix

--- client/client.lua
+++ client/client.lua
@@ -22,3 +22,12 @@ end)
 
 exports('getIsPlayerAnimal', function() return isPlayerAnimal end)
 -- DEBUG: print(exports['AnythingAnimal']:getIsPlayerAnimal())
+
+--Freeze stamina and make animal faster
+CreateThread(function()
+    while true do
+        Wait(0)
+        RestorePlayerStamina(PlayerId(), 1.0)
+        SetRunSprintMultiplierForPlayer(PlayerId(), 1.49)
+    end
+end)
