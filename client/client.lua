4585aa0eb280d76f5737534f3d62a1dbb4245a06
GeekGarage <info@geekgarage.dk>
2022-12-27 05:26:22 +0100

    speed multiplier in water lowered

--- client/client.lua
+++ client/client.lua
@@ -37,7 +37,7 @@ CreateThread(function()
 
         if IsEntityInWater(ped) == 1 then -- If In Water
             SetPedCanRagdoll(ped, false) -- Disable ragdoll of animals in water
-            SetRunSprintMultiplierForPlayer(player, 0.85) -- Make animals slower in water
+            SetRunSprintMultiplierForPlayer(player, 0.45) -- Make animals slower in water
         else
             SetPedCanRagdoll(ped, true) -- Enable ragdoll again
             SetRunSprintMultiplierForPlayer(player, 1.49) -- Make animals faster on land
