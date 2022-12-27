c5669836a35aa4d614c454cd8081906561b3e5ca
GeekGarage <info@geekgarage.dk>
2022-12-27 01:06:35 +0100

    Basic file structure

---
+++ config.lua
@@ -0,0 +1,20 @@
+---------------------------------------------------------
+--| List all the animal PEDs you have on your server. |--
+--|      All basic GTA animals are listed below!      |--
+--|   Anyone with a model listed below will be able   |--
+--|       to use animal emotes but not human          |--
+---------------------------------------------------------
+
+local animalPed = {
+    "a_c_chop",
+    "a_c_coyote",
+    "a_c_husky",
+    "a_c_mtlion",
+    "a_c_poodle",
+    "a_c_pug",
+    "a_c_retriever",
+    "a_c_rottweiler",
+    "a_c_shepherd",
+    "a_c_westy",
+    "ig_geek"
+}
