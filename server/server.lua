2cbeab87e50c728d88114174331bbc1a6acf6b7b
GeekGarage <info@geekgarage.dk>
2022-12-28 07:22:31 +0100

    add server verification of walk speed

--- server/server.lua
+++ server/server.lua
@@ -1,1 +1,11 @@
--- coming soon
+RegisterNetEvent("VerifyEmoteSpeed", function(speed)
+    local ped = GetPlayerPed(source)
+
+    if speed > 1.75 then
+        speed = 1.75
+    elseif speed < 0.0 then
+        speed = 0.0
+    end
+
+    TriggerClientEvent("updatewalkspeed", ped, speed)
+end)
