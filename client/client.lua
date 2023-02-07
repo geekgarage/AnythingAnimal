d5dd7990c7dabbb0f51e94630ed794eea6dc428a
GeekGarage <info@geekgarage.dk>
2023-02-07 17:44:34 +0100

    upd

--- client/client.lua
+++ client/client.lua
@@ -172,7 +172,8 @@ CreateThread(function()
                 end
                 if IsControlPressed(0, 22) then
                     print("AA: Jumping")
-                    TaskClimb(ped,false)
+                    print(GetOffsetFromEntityInWorldCoords(ped, 2, 0, 0))
+                    -- GetSafeCoordForPed(x, y, z, false, 16)
                 end
             end
             Wait(0)
