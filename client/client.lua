99089c4d864ec098e795917f5e4aad1950e59002
GeekGarage <info@geekgarage.dk>
2023-02-08 22:24:31 +0100

    upd

--- client/client.lua
+++ client/client.lua
@@ -171,13 +171,13 @@ CreateThread(function()
                     end
                 end
                 if IsControlPressed(0, 22) then
-                    currentPEDCoords = GetOffsetFromEntityInWorldCoords(ped, 0.0, 1.0, 0.0)
-                    offsetPEDBool, offsetPEDCoords = GetSafeCoordForPed(currentPEDCoords.x, currentPEDCoords.y, currentPEDCoords.z, false, 16)
-                    print("Current: " .. GetEntityCoords(ped))
-                    print("Offset: vec3(" .. currentPEDCoords.x .. " " .. currentPEDCoords.y .. " " .. currentPEDCoords.z ..")")
-                    print("New: vec3(" .. offsetPEDCoords.x .. " " .. offsetPEDCoords.y .. " " .. offsetPEDCoords.z ..")")
-                    if offsetPEDBool then
-                        SetEntityCoords(ped, offsetPEDCoords.x, offsetPEDCoords.y, offsetPEDCoords.z, false, false, false, false)
+                    offsetPEDCoords = GetOffsetFromEntityInWorldCoords(ped, 0.0, 1.0, 0.0)
+                    newPEDBool, newPEDCoords = GetSafeCoordForPed(offsetPEDCoords.x, offsetPEDCoords.y, offsetPEDCoords.z, false, 16)
+                    print("C: " .. GetEntityCoords(ped))
+                    print("O: vec3(" .. offsetPEDCoords.x .. " " .. offsetPEDCoords.y .. " " .. offsetPEDCoords.z ..")")
+                    print("N: vec3(" .. newPEDCoords.x .. " " .. newPEDCoords.y .. " " .. newPEDCoords.z ..")")
+                    if newPEDBool then
+                        SetEntityCoords(ped, newPEDCoords.x, newPEDCoords.y, newPEDCoords.z, false, false, false, false)
                     end
                 end
             end
