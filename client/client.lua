c4488a9784d122284aac10277089e84d4454ef2c
GeekGarage <info@geekgarage.dk>
2023-02-08 22:15:18 +0100

    upd

--- client/client.lua
+++ client/client.lua
@@ -171,7 +171,7 @@ CreateThread(function()
                     end
                 end
                 if IsControlPressed(0, 22) then
-                    currentPEDCoords = GetOffsetFromEntityInWorldCoords(ped, 10.0, 0.0, 0.0)
+                    currentPEDCoords = GetOffsetFromEntityInWorldCoords(ped, 0.0, 2.0, 0.0)
                     offsetPEDBool, offsetPEDCoords = GetSafeCoordForPed(currentPEDCoords.x, currentPEDCoords.y, currentPEDCoords.z, false, 16)
                     print("Current: " .. GetEntityCoords(ped))
                     print("Offset: vec3(" .. currentPEDCoords.x .. " " .. currentPEDCoords.y .. " " .. currentPEDCoords.z ..")")
