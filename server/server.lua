59b5b516815dc48e477194cad0db3bec8a891785
GeekGarage <info@geekgarage.dk>
2023-02-10 15:09:36 +0100

    fix

--- server/server.lua
+++ server/server.lua
@@ -55,7 +55,9 @@ end)
 RegisterNetEvent('JumpPED', function(isAnimal, jumpCoords)
     if isAnimal then
         if jumpCoords then
+            local pedVel = GetEntityVelocity(source)
             SetEntityCoords(source, jumpCoords.x, jumpCoords.y, jumpCoords.z, false, false, false, false)
+            SetEntityVelocity(source, pedVel.x, pedVel.y, pedVel.z)
         else
             TriggerClientEvent('GetOffsetInWorld', source)
         end
